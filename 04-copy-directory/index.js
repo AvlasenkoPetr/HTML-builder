249d1d6741f9d6b9799c21edc85da636a4603d5d
AvlasenkoPetr <phone.petr@gmail.com>
2021-11-10 18:03:17 +0300

    fix: new solution of fifth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,7 +6,6 @@ const fsProm = require('fs/promises');
 async function func() {
     try {
         await fsProm.rm(path.join(__dirname, 'files_copy'), { force: true, recursive: true, })
-        // await fsProm.stat(path.join(__dirname, 'files_copy'))
         await fsProm.mkdir(path.join(__dirname, 'files_copy'))
 
     } catch(err) {
@@ -22,36 +21,5 @@ async function func() {
         }
     }
 }
-// async function func() {
-//     await fs.stat(path.join(__dirname, 'files_copy'), function(err) {
-//         if (!err) {
-
-//             fs.rm(path.join(__dirname, 'files_copy'), { force: true, recursive: true, }, function() {
-//                 func()
-//             })
-
-//         } else  {
-//             fs.mkdir(path.join(__dirname, 'files_copy'), err => {
-//                 if(err) throw err;
-//                 console.log('Папка успешно создана');
-//             });
-//         }
-//     })
-
-//     await fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, function(err, files) {
-//         if (err) {
-//             throw err
-//         } else {
-//             for (file of files) {
-
-//                 fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files_copy', file.name), function(err) {
-//                     if (err) throw err
-//                 })
-//             }
-//         }
-//     })
-// }
 
 func()
-
-// Только на 6 работе окончательно разобрался с асинхронностьюовсем, но не было времени переделать 3-5 работы, сделаю в среду вечером, буду очень благодарен если перепроверите их потом
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,49 +1,43 @@
 const path = require('path');
 const fs = require('fs');
+const fsProm = require('fs/promises')
 
 async function func() {
-    fs.access(path.join(__dirname, 'project-dist', 'bundle.css'), fs.F_OK, function (err) {
-        if (err) {
-            fs.open(path.join(__dirname, 'project-dist', 'bundle.css'), 'w', (err) => {
-                if (err)
-                    throw err;
-                console.log('File created');
-            });
+    try {
+        try {
+            await fsProm.access(path.join(__dirname, 'project-dist', 'bundle.css'), fs.F_OK)
+            await fsProm.unlink(path.join(__dirname, 'project-dist', 'bundle.css'))
+        } catch {
+            console.log('File doesnt exist')
+        } finally {
+            await fsProm.open(path.join(__dirname, 'project-dist', 'bundle.css'), 'w')
+        }
+        
+    } catch (err) {
+        throw err
+    } finally {
+        fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, function (err, files) {
+            if (err) {
+                throw err;
+            } else {
+                for (file of files) {
+                    let ext = path.extname(path.join(__dirname, file.name));
 
-            fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, function (err, files) {
-                if (err) {
-                    throw err;
-                } else {
-                    for (file of files) {
-                        let ext = path.extname(path.join(__dirname, file.name));
-
-                        if (ext != '.css') {
-                            console.log('Игнорим: ', file.name);
-                            continue;
-                        } else {
-                            fs.readFile(path.join(__dirname, 'styles', file.name), function (err, data) {
-                                if (err) console.log(err);
-                                fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), `${data}\n`, function(err) {
-                                    if (err) throw err
-                                }) 
-                              });
-                        }
+                    if (ext != '.css') {
+                        console.log('Игнорим: ', file.name);
+                        continue;
+                    } else {
+                        fs.readFile(path.join(__dirname, 'styles', file.name), function (err, data) {
+                            if (err) console.log(err);
+                            fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), `${data}\n`, function(err) {
+                                if (err) throw err
+                            }) 
+                          });
                     }
                 }
-            });
-
-        } else {
-            fs.unlink(path.join(__dirname, 'project-dist', 'bundle.css'), (err) => {
-                if (err)
-                    throw err;
-                console.log('File deleted successfully!');
-
-                func();
-            });
-        }
-    })
+            }
+        });
+    }
 }
 
 func()
-
-// Только на 6 работе окончательно разобрался с асинхронностьюовсем, но не было времени переделать 3-5 работы, сделаю в среду вечером, буду очень благодарен если перепроверите их потом
