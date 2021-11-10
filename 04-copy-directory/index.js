510abdfd92ac337b12b029fbd0c375af1df3aea1
AvlasenkoPetr <phone.petr@gmail.com>
2021-11-10 17:27:41 +0300

    fix: new solution of fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,36 +1,56 @@
 const process = require('process');
 const path = require('path');
 const fs = require('fs');
+const fsProm = require('fs/promises');
 
 async function func() {
-        await fs.stat(path.join(__dirname, 'files_copy'), function(err) {
-            if (!err) {
+    try {
+        await fsProm.rm(path.join(__dirname, 'files_copy'), { force: true, recursive: true, })
+        // await fsProm.stat(path.join(__dirname, 'files_copy'))
+        await fsProm.mkdir(path.join(__dirname, 'files_copy'))
 
-                fs.rm(path.join(__dirname, 'files_copy'), { force: true, recursive: true, }, function() {
-                    func()
-                })
-
-            } else  {
-                fs.mkdir(path.join(__dirname, 'files_copy'), err => {
-                    if(err) throw err;
-                    console.log('Папка успешно создана');
-                });
-            }
-        })
-
-        await fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, function(err, files) {
-            if (err) {
+    } catch(err) {
+        throw err
+    } finally {
+        const files = await fsProm.readdir(path.join(__dirname, 'files'), {withFileTypes: true})
+        for (file of files) {
+            try {
+                await fsProm.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files_copy', file.name))
+            } catch (err) {
                 throw err
-            } else {
-                for (file of files) {
-
-                    fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files_copy', file.name), function(err) {
-                        if (err) throw err
-                    })
-                }
-            }
-        })
+            } 
+        }
+    }
 }
+// async function func() {
+//     await fs.stat(path.join(__dirname, 'files_copy'), function(err) {
+//         if (!err) {
+
+//             fs.rm(path.join(__dirname, 'files_copy'), { force: true, recursive: true, }, function() {
+//                 func()
+//             })
+
+//         } else  {
+//             fs.mkdir(path.join(__dirname, 'files_copy'), err => {
+//                 if(err) throw err;
+//                 console.log('Папка успешно создана');
+//             });
+//         }
+//     })
+
+//     await fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, function(err, files) {
+//         if (err) {
+//             throw err
+//         } else {
+//             for (file of files) {
+
+//                 fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files_copy', file.name), function(err) {
+//                     if (err) throw err
+//                 })
+//             }
+//         }
+//     })
+// }
 
 func()
 
